e26091f63815daca97c9b28410924389ea78c91a
Josh Rosenfeld <joshr@rogers.com>
2024-01-01 22:44:31 -0500

    refactor: refactor api actions from fetch to axios

--- client/src/api/actions.ts
+++ client/src/api/actions.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { PostData } from "../components/Form/Form";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { ObjectId } from "mongodb";
@@ -5,54 +6,17 @@ import { ObjectId } from "mongodb";
 const url = 'http://localhost:3000/posts'
 
 export const getPosts = createAsyncThunk('posts/getPosts', async () => {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log((error as Error).message);
-    }
+    const response = await axios.get(url);
+    return response.data
 })
 
 export const createPost = createAsyncThunk('posts/createPosts', async (createdPost: PostData) => {
-    try {
-        const config = {
-            method: "POST",
-            headers: {
-              Application: "application/json",
-              "Content-Type": "application/json",
-            },
-            // Expected Payload (The request body)
-            body: JSON.stringify({
-              author: createdPost.author,
-              title: createdPost.title,
-              message: createdPost.message,
-              tags: createdPost.tags,
-              selectedFile: createdPost.selectedFile
-            }),
-          };
-    
-          const response = await fetch(url, config);
-          const data = await response.json();
-          return data;
-    } catch (error) {
-        console.log((error as Error).message);
-    }
+    const response = await axios.post(url, createdPost);
+    return response.data;
 })
 
+
 export const deletePost = createAsyncThunk('posts/deletePost', async (id: ObjectId) => {
-    try {
-        const config = {
-            method: "DELETE",
-            headers: {
-              Application: "application/json",
-              "Content-Type": "application/json",
-            }}
-            
-        const response = await fetch(`${url}/${id}`, config);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log((error as Error).message);
-    }
+    const response = await axios.delete(`${url}/${id}`);
+    return response.data;
 })
